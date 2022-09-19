458f19aa09f83af7774fbc08553fa8f526f4fd52
Kevin Quick <kquick@galois.com>
2022-09-18 22:24:44 -0700

    Implement search strategy for Projects list pane.

--- app/Panes/Projects.hs
+++ app/Panes/Projects.hs
@@ -14,6 +14,7 @@ import           Brick.Widgets.List
 import           Control.Lens
 import qualified Data.Sequence as Seq
 import           Data.Text ( Text )
+import qualified Data.Text as T
 import qualified Data.Text.Zipper as TZ
 import qualified Data.Vector as V
 
@@ -23,15 +24,17 @@ import           Defs
 instance Pane WName MyWorkEvent Projects Projects where
   data (PaneState Projects MyWorkEvent) = P { pL :: List WName (Role, Text)
                                             , pS :: Editor Text WName
+                                            , oC :: [ (Role, Text) ]
                                             }
   type (InitConstraints Projects s) = ( HasProjects s )
   type (DrawConstraints Projects s WName) = ( HasFocus s WName )
   type (EventType Projects WName MyWorkEvent) = BrickEvent WName MyWorkEvent
   initPaneState s =
     let prjs = projects $ snd $ getProjects s
-        pl = list (WName "Projs:List") (V.fromList (mkListEnt <$> prjs)) 1
+        oc = mkListEnt <$> prjs
+        pl = list (WName "Projs:List") (V.fromList oc) 1
         ps = editor (WName "Projs:Filter") (Just 1) ""
-    in P pl ps
+    in P pl ps oc
   drawPane ps gs =
     let isFcsd = gs^.getFocus.to focused == Just (WName "Pane:ProjList")
         renderListEnt _ (r,n) = withAttr (roleAttr r) $ txt n
@@ -43,13 +46,24 @@ instance Pane WName MyWorkEvent Projects Projects where
                 VtyEvent ev' ->
                   ps & pList %%~ \w -> nestEventM' w (handleListEvent ev')
                 _ -> return ps
+       let origSearchText = head $ ps ^. pSrch . to getEditContents
        ps2 <- ps1 & pSrch %%~ \w -> nestEventM' w (handleEditorEvent ev)
-       return ps2
+       let searchText = head $ ps2 ^. pSrch . to getEditContents
+       if searchText == origSearchText
+         then return ps2
+         else let nc = if T.null searchText
+                       then oC ps2
+                       else filter ((searchText `T.isInfixOf`) . snd) (oC ps2)
+                  np = if null nc then Nothing else Just 0
+              in return $ ps2 & pList %~ listReplace (V.fromList nc) np
   focusable _ _ = Seq.singleton (WName "Pane:ProjList")
   updatePane newprjs =
-    (pList %~ listReplace (V.fromList (mkListEnt <$> projects newprjs)) (Just 0))
-    .
-    (pSrch . editContentsL %~ TZ.clearZipper)
+    let oc = mkListEnt <$> projects newprjs
+    in (pList %~ listReplace (V.fromList oc) (Just 0))
+       .
+       (pSrch . editContentsL %~ TZ.clearZipper)
+       .
+       (pOrig .~ oc)
 
 
 pList :: Lens' (PaneState Projects MyWorkEvent) (List WName (Role, Text))
@@ -57,6 +71,9 @@ pList f ps = (\n -> ps { pL = n }) <$> f (pL ps)
 
 pSrch :: Lens' (PaneState Projects MyWorkEvent) (Editor Text WName)
 pSrch f ps = (\n -> ps { pS = n }) <$> f (pS ps)
+
+pOrig :: Lens' (PaneState Projects MyWorkEvent) [ (Role, Text) ]
+pOrig f ps = (\n -> ps { oC = n }) <$> f (oC ps)
 
 mkListEnt :: Project -> (Role, Text)
 mkListEnt pr = (role pr, name pr)
